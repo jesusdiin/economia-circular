b939d0e0bc50dd7df1655cf66acbff4423d861a8
Jesùs <cjesus02hdz@gmail.com>
2025-11-22 19:55:24 -0600

    Revert "update"
    This reverts commit e456ee656918f242e480e9e928f74f8defa81ff1.

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,13 @@
 import express from "express";
 import recicladoras from "./api/recicladora";
+import 'dotenv/config';
+
 
 const app = express();
+
 app.use("/api/recicladoras", recicladoras);
 
-if (process.env.NODE_ENV !== "production") {
-  const PORT = 3000;
-  app.listen(PORT, () => {
-    console.log(`Dev server on http://localhost:${PORT}`);
-  });
-}
-
-export default app;
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Dev server running on http://localhost:${PORT}`);
+});
